6eb695586c6f0970ec23467ca02f459aac036918
Rafael Pessoa <152654760+TheRPessoa@users.noreply.github.com>
2024-11-25 09:25:13 -0300

    Add files via upload

---
+++ GravadorA-stable/README.md
@@ -0,0 +1,1 @@
+# GravadorA <img title="" src="app/src/main/res/drawable/GravadorA.png" align="left" width="64" style="padding: 12px;">
---
+++ GravadorA-stable/app/src/main/java/com/gravadora/app/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.gravadora.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //modo noturno
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
